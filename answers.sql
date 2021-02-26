6087cf60361932ed4dc6115982a2876906be42fc
Jonathas Tavares <jonathastavares@id.uff.br>
2021-02-26 10:52:01 -0300

    SELECT quiz done

---
+++ answers.sql
@@ -0,0 +1,58 @@
+/* SELECT Quiz */
+
+/* 1 */
+
+SELECT name, population FROM world WHERE population BETWEEN 1000000 AND 1250000;
+
+/* 2 */
+
+/* Table - E */
+
+/* 3 */
+
+SELECT name FROM world WHERE name LIKE '%a' OR name LIKE '%l'
+
+/* 4 */
+
+/* Table - C */
+
+/* 5 */
+
+/* Andorra - 936 */
+
+/* 6 */
+
+SELECT name, area, population FROM world WHERE area > 50000 AND population < 10000000
+
+/* 7 */
+
+SELECT name, population/area FROM world WHERE name IN ('China', 'Nigeria', 'France', 'Australia')
+
+/*----------------------------------------------------------------------------*/
+
+/* BBC Quiz */
+
+/* 1 */
+
+SELECT name FROM world WHERE name LIKE 'U%'
+
+/* 2 */
+
+SELECT population FROM 'United Kingdom'
+
+/* 3 */
+
+/* 'name' should be name */
+
+/* 4 */
+
+
+
+/* 5 */
+
+
+/* 6 */
+
+
+/* 7 */
+
