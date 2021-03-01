04027fa5fb18d80d367283078a90ceac401bef16
Jonathas Tavares <jonathastavares@id.uff.br>
2021-03-01 11:54:53 -0300

    Nobel quiz done

--- answers.sql
+++ answers.sql
@@ -56,3 +56,35 @@ SELECT population FROM 'United Kingdom'
 
 /* 7 */
 
+
+/*----------------------------------------------------------------------------*/
+
+/* NOBEL Quiz */
+
+/* 1 */
+
+SELECT winner FROM nobel WHERE winner LIKE 'C%' AND winner LIKE '%n'
+
+/* 2 */
+
+SELECT COUNT(subject) FROM nobel WHERE subject = 'Chemistry' AND yr BETWEEN 1950 and 1960
+
+/* 3 */
+
+SELECT COUNT(DISTINCT yr) FROM nobel WHERE yr NOT IN (SELECT DISTINCT yr FROM nobel WHERE subject = 'Medicine')
+
+/* 4 */
+
+/* Alternative C */
+
+/* 5 */
+
+SELECT yr FROM nobel WHERE yr NOT IN(SELECT yr FROM nobel WHERE subject IN ('Chemistry','Physics'))
+
+/* 6 */
+
+SELECT DISTINCT yr FROM nobel WHERE subject='Medicine' AND yr NOT IN(SELECT yr FROM nobel WHERE subject='Literature') AND yr NOT IN (SELECT yr FROM nobel WHERE subject='Peace')
+
+/* 7 */
+
+/* Alternative D */
